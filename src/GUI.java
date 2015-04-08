ad6deb1b8ddd4718b82ac300d54a1dc682753d81
AndersSperling <anderssperling@gmail.com>
2015-04-08 17:25:08 +0200

    LowerCase
    h

--- src/GUI.java
+++ src/GUI.java
@@ -90,7 +90,7 @@ public class GUI extends JFrame implements MouseListener, KeyListener {
 		// TODO Auto-generated method stub
 		if(e.getKeyCode() == KeyEvent.VK_ENTER){
 			//System.out.println(editText.getText());
-			i.search(editText.getText());
+			i.search(editText.getText().toLowerCase());
 			editText.setText(null);
 		}
 		
